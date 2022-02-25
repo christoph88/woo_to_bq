b8ea1453e0ee8357c3ffe54818a54068c146bc51
Christoph Geypen <geypenchristoph@gmail.com>
2022-02-25 19:08:38 +0100

    add proper template for testing

--- test-local.sh
+++ test-local.sh
@@ -2,5 +2,14 @@
 
 export GOOGLE_APPLICATION_CREDENTIALS="/Users/christophgeypen/code/supervers/woo_to_bq.json" 
 
-# node -r dotenv-yaml/config -e 'require("./index.js").run({"query": {"page":1}}, null)' dotenv_yaml_config_path=./.env.yaml dotenv_yaml_config_encoding=utf8
-node -r dotenv-yaml/config -e 'require("./index.js").run({"query": {"getAmountOfPages":"true"}}, null)' dotenv_yaml_config_path=./.env.yaml dotenv_yaml_config_encoding=utf8
+# get table
+# node -r dotenv-yaml/config -e 'require("./index.js").getTable({"params": {"datasetId": "budgets", "tableId": "join_allocations_and_spends_static"}}, null)' dotenv_yaml_config_path=./mv.env.yaml dotenv_yaml_config_encoding=utf8
+
+# get view
+# node -r dotenv-yaml/config -e 'require("./index.js").getView({"params": {"datasetId": "budgets", "viewId": "allocations"}}, null)' dotenv_yaml_config_path=./mv.env.yaml dotenv_yaml_config_encoding=utf8
+
+# get tables from dataset
+# node -r dotenv-yaml/config -e 'require("./index.js").getDataset({"params": {"datasetId": "budgets"}}, null)' dotenv_yaml_config_path=./mv.env.yaml dotenv_yaml_config_encoding=utf8
+
+# do backup
+# node -r dotenv-yaml/config -e 'require("./index.js").getBackup({"params": {}}, null)' dotenv_yaml_config_path=./mv.env.yaml dotenv_yaml_config_encoding=utf8
