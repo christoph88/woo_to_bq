6f8ecd5dadb8bd338f457671ba5e922d7da6ccd7
Christoph Geypen <geypenchristoph@gmail.com>
2022-02-25 19:33:18 +0100

    set proper path to key

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export GOOGLE_APPLICATION_CREDENTIALS="/Users/christophgeypen/code/supervers/woo_to_bq.json" 
+export GOOGLE_APPLICATION_CREDENTIALS="/Users/christophgeypen/keys/woo_to_bq.json" 
 
 # get order page
 node -r dotenv-yaml/config -e 'require("./index.js").getOrderPage({"params": {"page": "2"}}, null)' dotenv_yaml_config_path=./.env.yaml dotenv_yaml_config_encoding=utf8
